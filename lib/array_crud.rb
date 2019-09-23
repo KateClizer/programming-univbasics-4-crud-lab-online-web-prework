e5141c4b150866b28b709315ebe2a02b439e5b13
Katelynne Clizer <kateclizer@gmail.com>
2019-09-23 20:32:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,7 +25,7 @@ end
 
 def remove_element_from_start_of_array(array)
   clizer_family = ["wow", "Katelynne", "Chase", "arrays!"]
-clizer_family.shift 
+wow = clizer_family.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
