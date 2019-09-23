86b796ad50875ddafbca28a96c3130f641232009
Katelynne Clizer <kateclizer@gmail.com>
2019-09-23 20:30:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,31 @@
 def create_an_empty_array
+  [ ]
   
 end
 
 def create_an_array
+  clizer_family = ["Kyle", "Katelynne", "Chase", "Judson"]
   
 end
 
 def add_element_to_end_of_array(array, element)
-  
+ clizer_family = ["Kyle", "Katelynne", "Chase", "Judson"]
+ clizer_family << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  clizer_family = ["Kyle", "Katelynne", "Chase", "Judson"]
+ clizer_family.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  clizer_family = ["Kyle", "Katelynne", "Chase", "arrays!"]
+clizer_family.pop 
 end
 
 def remove_element_from_start_of_array(array)
-  
+  clizer_family = ["wow", "Katelynne", "Chase", "arrays!"]
+clizer_family.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
